bdb0ea2b8fdc92ee2b5cdd46b777fb71ed1bbdce
Prajith Nair <kprajith@amazon.com>
2018-11-17 14:17:02 +0000

    adding verbose

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const run = async () => {
     const args = parseArgs(process.argv.slice(2), {
         string: 'splitBy',
         string: 'take',
+        boolean: 'verbose',
         alias:  {
             s: 'splitBy',
-            t: 'take'
+            t: 'take',
+            v: 'verbose'
         },
-        default: { splitBy: ' ', take: '0' }
+        default: { splitBy: ' ', take: '0', verbose: false }
     });
     const selector = {
         splitBy: args.splitBy,
-        indexToTake: parseInt(args.take)
+        indexToTake: parseInt(args.take),
+        verbose: args.verbose 
     }
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
@@ -26,11 +29,15 @@ const run = async () => {
     });
 
     rl.on('line', function(input){
-        const result = input.split(selector.splitBy).find((v, index) => {
+        const tokens = input.split(selector.splitBy);
+        const result = tokens.find((v, index) => {
             if (index == selector.indexToTake){
                 return v;
             }
         })
+        if(selector.verbose) {
+            console.log(tokens.map((v, i) => `[index: ${i}| value: ${v}]`).join());
+        }
         if( result ){
             process.stdout.write(`${result}\n`)
         } 
